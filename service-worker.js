6765896fda44b5fb1098321af3df1d0860aedd5a
proc0x41 <arthurmendesgarcia@gmail.com>
2025-03-15 13:18:52 +0000

    aaa

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'pwa-iframe-cache-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/icon-192x192.png',
+  '/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
